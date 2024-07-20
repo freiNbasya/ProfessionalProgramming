ce9d93c17af7c0ddaa64fab6a54ef4506ffb21d1
shito.bulo@gmail.com <shito.bulo@gmail.com>
2024-07-20 21:51:06 +0300

    added publish fix

--- home6.1/MsgDispatcher.java
+++ home6.1/MsgDispatcher.java
@@ -1,52 +1,97 @@
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
-public class MsgDispatcher {
-    private final ConcurrentHashMap<Integer, Consumer<GreenMsg>> greenMsgSubscribers = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<Integer, Consumer<BlueMsg>> blueMsgSubscribers = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<Integer, Consumer<OrangeMsg>> orangeMsgSubscribers = new ConcurrentHashMap<>();
+public class MessageDispatcher {
+    private final ConcurrentHashMap<Integer, Consumer<GreenMessage>> greenMessageSubscribers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, Consumer<BlueMessage>> blueMessageSubscribers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, Consumer<OrangeMessage>> orangeMessageSubscribers = new ConcurrentHashMap<>();
+    private final BlockingQueue<GreenMessage> greenMessageQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<BlueMessage> blueMessageQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<OrangeMessage> orangeMessageQueue = new LinkedBlockingQueue<>();
     private final AtomicInteger subscriberId = new AtomicInteger(0);
 
-    public int subscribeToGreenMsg(Consumer<GreenMsg> subscriber) {
+    public MessageDispatcher() {
+        startProcessing();
+    }
+
+    private void startProcessing() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    GreenMessage message = greenMessageQueue.take();
+                    for (Consumer<GreenMessage> subscriber : greenMessageSubscribers.values()) {
+                        subscriber.accept(message);
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    BlueMessage message = blueMessageQueue.take();
+                    for (Consumer<BlueMessage> subscriber : blueMessageSubscribers.values()) {
+                        subscriber.accept(message);
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    OrangeMessage message = orangeMessageQueue.take();
+                    for (Consumer<OrangeMessage> subscriber : orangeMessageSubscribers.values()) {
+                        subscriber.accept(message);
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }).start();
+    }
+
+    public int subscribeToGreenMessage(Consumer<GreenMessage> subscriber) {
         int id = subscriberId.incrementAndGet();
-        greenMsgSubscribers.put(id, subscriber);
+        greenMessageSubscribers.put(id, subscriber);
         return id;
     }
 
-    public int subscribeToBlueMsg(Consumer<BlueMsg> subscriber) {
+    public int subscribeToBlueMessage(Consumer<BlueMessage> subscriber) {
         int id = subscriberId.incrementAndGet();
-        blueMsgSubscribers.put(id, subscriber);
+        blueMessageSubscribers.put(id, subscriber);
         return id;
     }
 
-    public int subscribeToOrangeMsg(Consumer<OrangeMsg> subscriber) {
+    public int subscribeToOrangeMessage(Consumer<OrangeMessage> subscriber) {
         int id = subscriberId.incrementAndGet();
-        orangeMsgSubscribers.put(id, subscriber);
+        orangeMessageSubscribers.put(id, subscriber);
         return id;
     }
 
     public void unsubscribe(int id) {
-        greenMsgSubscribers.remove(id);
-        blueMsgSubscribers.remove(id);
-        orangeMsgSubscribers.remove(id);
+        greenMessageSubscribers.remove(id);
+        blueMessageSubscribers.remove(id);
+        orangeMessageSubscribers.remove(id);
     }
 
-    public void publishGreenMsg(GreenMsg message) {
-        for (Consumer<GreenMsg> subscriber : greenMsgSubscribers.values()) {
-            subscriber.accept(message);
-        }
+    public void publishGreenMessage(GreenMessage message) {
+        greenMessageQueue.offer(message);
     }
 
-    public void publishBlueMsg(BlueMsg message) {
-        for (Consumer<BlueMsg> subscriber : blueMsgSubscribers.values()) {
-            subscriber.accept(message);
-        }
+    public void publishBlueMessage(BlueMessage message) {
+        blueMessageQueue.offer(message);
     }
 
-    public void publishOrangeMsg(OrangeMsg message) {
-        for (Consumer<OrangeMsg> subscriber : orangeMsgSubscribers.values()) {
-            subscriber.accept(message);
-        }
+    public void publishOrangeMessage(OrangeMessage message) {
+        orangeMessageQueue.offer(message);
     }
 }
